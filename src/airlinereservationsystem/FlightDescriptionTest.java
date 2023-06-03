7b7f50d8da47d1baf38f6b0669619f1ae685159e
Raquel Alves <51912497+RaquelABrito@users.noreply.github.com>
2023-06-03 18:49:22 -0300

    delete test

--- src/airlinereservationsystem/FlightDescriptionTest.java
+++ src/airlinereservationsystem/FlightDescriptionTest.java
@@ -1,23 +1,21 @@
 package airlinereservationsystem;
-package airlinereservationsystem;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-public class FlightDescriptionTest {
-	
+class FlightDescriptionTest {
+
 	@Test
 	public void checkTimeBadFormatLimiteSuperior() {
-		FligtDescripton flight = new FlightDescription();
-		resp = flight.check_time("24:00");
+		boolean resp = FlightDescription.check_time("23:60");
 		assertEquals(false, resp); 
 	}
 	
 	@Test
 	public void checkTimeBadFormatLimiteInferior() {
-		FligtDescripton flight = new FlightDescription();
-		resp = flight.check_time("-01:00");
+		boolean resp = FlightDescription.check_time("-01:00");
 		assertEquals(false, resp); 
 	}
+
 }
