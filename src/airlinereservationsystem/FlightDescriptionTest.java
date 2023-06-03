4f8864929b4c1ece0beff9346d2671965562705f
Raquel Alves <51912497+RaquelABrito@users.noreply.github.com>
2023-06-03 19:14:57 -0300

    add tests

--- src/airlinereservationsystem/FlightDescriptionTest.java
+++ src/airlinereservationsystem/FlightDescriptionTest.java
@@ -7,6 +7,12 @@ import org.junit.jupiter.api.Test;
 class FlightDescriptionTest {
 
 	@Test
+	public void checkTimeBadFormatCorrect() {
+		boolean resp = FlightDescription.check_time("23:59");
+		assertEquals(true, resp); 
+	}
+	
+	@Test
 	public void checkTimeBadFormatLimiteSuperior() {
 		boolean resp = FlightDescription.check_time("23:60");
 		assertEquals(false, resp); 
@@ -14,8 +20,38 @@ class FlightDescriptionTest {
 	
 	@Test
 	public void checkTimeBadFormatLimiteInferior() {
-		boolean resp = FlightDescription.check_time("-01:00");
+		boolean resp = FlightDescription.check_time("-1:00");
 		assertEquals(false, resp); 
 	}
+	
+	@Test
+	public void checkTimeBadFormatMinutoInseridoErrado() {
+		boolean resp = FlightDescription.check_time("01:0");
+		assertEquals(false, resp); 
+	}
+	
+	@Test
+	public void checkTimeBadFormatoInseridoErrado() {
+		boolean resp = FlightDescription.check_time("1");
+		assertEquals(false, resp); 
+	}
+	
+//	@Test
+//	public void checkTimeBadFormatoInseridoErradoCaracteres() {
+//		boolean resp = FlightDescription.check_time("01:0010");
+//		assertEquals(false, resp); 
+//	}
+//	
+//	@Test
+//	public void checkTimeBadFormatoInseridoCertoCaracteres() {
+//		boolean resp = FlightDescription.check_time("01:001");
+//		assertEquals(false, resp); 
+//	}
+//	
+//	@Test
+//	public void checkTimeBadFormatoInseridoErradoCaracteresMenor() {
+//		boolean resp = FlightDescription.check_time("123");
+//		assertEquals(false, resp); 
+//	}
 
 }
