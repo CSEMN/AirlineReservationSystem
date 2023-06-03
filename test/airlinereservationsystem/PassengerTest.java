1e896844a61d4932a41c9906d46dc8a999178ab5
Raquel Alves <51912497+RaquelABrito@users.noreply.github.com>
2023-06-03 13:42:25 -0300

    tests junit
    create junit test package

---
+++ test/airlinereservationsystem/PassengerTest.java
@@ -0,0 +1,14 @@
+package airlinereservationsystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PassengerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/airlinereservationsystem/ProjectDBTest.java
@@ -0,0 +1,14 @@
+package airlinereservationsystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProjectDBTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
